d3aa9866a2126dc6a84ec736ffaa9b8395c5b691
Anurag Rajput <code.annu@gmail.com>
2026-02-05 08:51:20 +0530

    add di layer for tracks, albums and artists

---
+++ backend/src/di/container.ts
@@ -0,0 +1,106 @@
+import { Container } from "inversify";
+import "reflect-metadata";
+
+import { TYPES } from "./types";
+
+// Repository interfaces
+import {
+  IArtistRepository,
+  IAlbumRepository,
+  ITrackRepository,
+} from "../domain/repositories";
+
+// Repository implementations
+import {
+  PrismaArtistRepository,
+  PrismaAlbumRepository,
+  PrismaTrackRepository,
+} from "../infrastructure/repositories";
+
+// Use cases
+import {
+  GetTrackByIdUseCase,
+  GetRandomTracksUseCase,
+} from "../application/usecases/track";
+import {
+  GetAlbumByIdUseCase,
+  GetRandomAlbumsUseCase,
+} from "../application/usecases/album";
+import {
+  GetArtistByIdUseCase,
+  GetRandomArtistsUseCase,
+} from "../application/usecases/artist";
+
+// Controllers
+import { TrackController } from "../api/controllers/TrackController";
+import { AlbumController } from "../api/controllers/AlbumController";
+import { ArtistController } from "../api/controllers/ArtistController";
+
+/**
+ * Create and configure the Inversify DI container
+ */
+function createContainer(): Container {
+  const container = new Container();
+
+  // Bind Repositories
+  container
+    .bind<IArtistRepository>(TYPES.ArtistRepository)
+    .to(PrismaArtistRepository)
+    .inSingletonScope();
+  container
+    .bind<IAlbumRepository>(TYPES.AlbumRepository)
+    .to(PrismaAlbumRepository)
+    .inSingletonScope();
+  container
+    .bind<ITrackRepository>(TYPES.TrackRepository)
+    .to(PrismaTrackRepository)
+    .inSingletonScope();
+
+  // Bind Track Use Cases
+  container
+    .bind<GetTrackByIdUseCase>(TYPES.GetTrackByIdUseCase)
+    .to(GetTrackByIdUseCase)
+    .inSingletonScope();
+  container
+    .bind<GetRandomTracksUseCase>(TYPES.GetRandomTracksUseCase)
+    .to(GetRandomTracksUseCase)
+    .inSingletonScope();
+
+  // Bind Album Use Cases
+  container
+    .bind<GetAlbumByIdUseCase>(TYPES.GetAlbumByIdUseCase)
+    .to(GetAlbumByIdUseCase)
+    .inSingletonScope();
+  container
+    .bind<GetRandomAlbumsUseCase>(TYPES.GetRandomAlbumsUseCase)
+    .to(GetRandomAlbumsUseCase)
+    .inSingletonScope();
+
+  // Bind Artist Use Cases
+  container
+    .bind<GetArtistByIdUseCase>(TYPES.GetArtistByIdUseCase)
+    .to(GetArtistByIdUseCase)
+    .inSingletonScope();
+  container
+    .bind<GetRandomArtistsUseCase>(TYPES.GetRandomArtistsUseCase)
+    .to(GetRandomArtistsUseCase)
+    .inSingletonScope();
+
+  // Bind Controllers
+  container
+    .bind<TrackController>(TYPES.TrackController)
+    .to(TrackController)
+    .inSingletonScope();
+  container
+    .bind<AlbumController>(TYPES.AlbumController)
+    .to(AlbumController)
+    .inSingletonScope();
+  container
+    .bind<ArtistController>(TYPES.ArtistController)
+    .to(ArtistController)
+    .inSingletonScope();
+
+  return container;
+}
+
+export const container = createContainer();
---
+++ backend/src/di/types.ts
@@ -0,0 +1,26 @@
+/**
+ * Inversify DI Type Symbols
+ */
+export const TYPES = {
+  // Repositories
+  ArtistRepository: Symbol.for("ArtistRepository"),
+  AlbumRepository: Symbol.for("AlbumRepository"),
+  TrackRepository: Symbol.for("TrackRepository"),
+
+  // Use Cases - Track
+  GetTrackByIdUseCase: Symbol.for("GetTrackByIdUseCase"),
+  GetRandomTracksUseCase: Symbol.for("GetRandomTracksUseCase"),
+
+  // Use Cases - Album
+  GetAlbumByIdUseCase: Symbol.for("GetAlbumByIdUseCase"),
+  GetRandomAlbumsUseCase: Symbol.for("GetRandomAlbumsUseCase"),
+
+  // Use Cases - Artist
+  GetArtistByIdUseCase: Symbol.for("GetArtistByIdUseCase"),
+  GetRandomArtistsUseCase: Symbol.for("GetRandomArtistsUseCase"),
+
+  // Controllers
+  TrackController: Symbol.for("TrackController"),
+  AlbumController: Symbol.for("AlbumController"),
+  ArtistController: Symbol.for("ArtistController"),
+};
